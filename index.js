e2fb468f612c8e7b6855355fa390796d19b511cf
Ever <redsoul10@hotmail.com>
2021-04-19 16:36:20 -0300

    added put, post and git ignore

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const port = 4000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+newUser = {
+  email: "",
+  fullName: "",
+  password: ""
+}
+
+app.post('/userRegistration', (req, res) => {
+  newUser.email= req.body.email
+  newUser.fullName=req.body.fullName
+  newUser.password=req.body.password
+  console.log(newUser);
+  res.send({userRegistered: newUser});
+})
+
+app.put('/userLogin', (req, res) => {
+  const userVerification = {
+    email: '',
+    password: ''
+  }
+  userVerification.email = req.body.email
+  userVerification.password = req.body.password
+
+  if (userVerification.email === newUser.email && userVerification.password == newUser.password) {
+    res.send({ logged: userVerification })
+  }
+  else {
+    res.status(404).send({ error: 'Invalid user' })
+  }
 })
 
 app.listen(port, () => {
